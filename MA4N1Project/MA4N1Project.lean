f453c4afa7dbe7c127a56579144e14ab655dd3ea
Jack Atfield <109289488+J-Atfield@users.noreply.github.com>
2023-11-15 10:13:16 +0000

    fixed type

--- MA4N1Project/MA4N1Project.lean
+++ MA4N1Project/MA4N1Project.lean
@@ -8,7 +8,7 @@ open Polynomial
 
 def exists_infinitely_many_P : Prop := ∀ n : ℕ, ∃ p : ℕ, Nat.Prime p ∧ p > n
 
-lemma fundamental_lemma {f: Polynomial ℤ} (h : degree f > 0) : exists_infinitely_many_p ∧ (∃ x : ℤ, f.eval x ≡ 0 [ZMOD p]) := by
+lemma fundamental_lemma {f: Polynomial ℤ} (h : degree f > 0) : exists_infinitely_many_P ∧ (∃ x : ℤ, f.eval x ≡ 0 [ZMOD p]) := by
   sorry
   done
 
