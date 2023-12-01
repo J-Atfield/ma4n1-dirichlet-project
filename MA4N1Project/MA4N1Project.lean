5d3f60e4b6abc64d53eb494688eeae273e34edd3
Jack Atfield <109289488+J-Atfield@users.noreply.github.com>
2023-12-01 12:49:23 +0000

    proved iff relation for 4k+1, generalised to nk+1

--- MA4N1Project/MA4N1Project.lean
+++ MA4N1Project/MA4N1Project.lean
@@ -159,36 +159,11 @@ lemma rearrange {p k : ℕ} (h : Nat.Prime p) (hp : p % 4 = 1) : (p - 1) / 4 = k
   rw [← h3, ← h2]
   done
 
-theorem p_mod_4_eq_one_iff_p_eq_4k_plus_1 {p : ℕ} (hp : p.Prime) : (p % 4 = 1) ↔ (p = 4*k + 1) := by
-  apply Iff.intro
-  case mpr =>
-    intro h_4k_1
-    rw [h_4k_1, add_mod, mul_mod_right, zero_add, mod_mod]
-    exact rfl
-  case mp =>
-    intro hp_mod_4
-    have h_mod_equiv : 1 ≡ p [MOD 4] := by
-      rw [← hp_mod_4]
-      exact mod_modEq p 4
-    have h_four_div_p_minus_one : 4 ∣ (p - 1) := by
-      rw [← modEq_iff_dvd']
-      apply h_mod_equiv
-      refine one_le_iff_ne_zero.mpr ?_
-      exact Nat.Prime.ne_zero hp
-    have test : ∃ (k : ℕ), p-1=k*4 := by
-      apply exists_eq_mul_left_of_dvd
-      exact h_four_div_p_minus_one
-    have h_p_minus_one_eq_4k : p - 1 = 4*k := by
-      rw [mul_comm]
-
-    rw [← h_p_minus_one_eq_4k]
-    exact (succ_pred_prime hp).symm
-  done
-
 theorem p_mod_4_eq_one_iff_p_eq_4k_plus_1' {p : ℕ} (hp : p.Prime) : (p % 4 = 1) ↔ (∃ (k : ℕ), p = 4*k + 1) := by
   apply Iff.intro
   case mpr =>
-    intro h_4k_1
+    simp only [forall_exists_index]
+    intro k h_4k_1
     rw [h_4k_1, add_mod, mul_mod_right, zero_add, mod_mod]
     exact rfl
   case mp =>
@@ -201,31 +176,77 @@ theorem p_mod_4_eq_one_iff_p_eq_4k_plus_1' {p : ℕ} (hp : p.Prime) : (p % 4 = 1
       apply h_mod_equiv
       refine one_le_iff_ne_zero.mpr ?_
       exact Nat.Prime.ne_zero hp
-    have test : ∃ (k : ℕ), p-1=k*4 := by
+    have h_exists_k_p1_eq_k4 : ∃ (k : ℕ), p-1=k*4 := by
       apply exists_eq_mul_left_of_dvd
       exact h_four_div_p_minus_one
-
+    cases h_exists_k_p1_eq_k4 with
+    | intro k h =>
+      use k
+      rw [mul_comm]
+      have : 1 ≤ p := by
+      {
+        rw [@one_le_iff_ne_zero]
+        exact Nat.Prime.ne_zero hp
+      }
+      exact Nat.eq_add_of_sub_eq this h
   done
 
-theorem inf_p_4k_plus_one (hp : p.Prime) (hp2 : p > 2) (hs : IsSquare (-1 : ZMod p)) : exists_infinitely_many_P ∧ p = 4*k+1 := by
+theorem p_mod_n_eq_one_iff_p_eq_nk_plus_1' {p : ℕ} (hp : p.Prime) : (p % (n+2) = 1) ↔ (∃ (k : ℕ), p = (n+2)*k + 1) := by
+  apply Iff.intro
+  case mpr =>
+    simp only [forall_exists_index]
+    intro k h_nk_1
+    rw [h_nk_1, add_mod, mul_mod_right, zero_add, mod_mod]
+    apply one_mod
+  case mp =>
+    intro hp_mod_n_plus_2
+    have h_mod_equiv : 1 ≡ p [MOD (n+2)] := by
+      rw [← hp_mod_n_plus_2]
+      exact mod_modEq p (n+2)
+    have h_n_plus_2_div_p_minus_one : (n + 2) ∣ (p - 1) := by
+      rw [← modEq_iff_dvd']
+      apply h_mod_equiv
+      refine one_le_iff_ne_zero.mpr ?_
+      exact Nat.Prime.ne_zero hp
+    have h_exists_k_p1_eq_kn : ∃ (k : ℕ), p-1=k*(n+2) := by
+      apply exists_eq_mul_left_of_dvd
+      exact h_n_plus_2_div_p_minus_one
+    cases h_exists_k_p1_eq_kn with
+    | intro k h =>
+      use k
+      rw [mul_comm]
+      have : 1 ≤ p := by
+      {
+        rw [@one_le_iff_ne_zero]
+        exact Nat.Prime.ne_zero hp
+      }
+      exact Nat.eq_add_of_sub_eq this h
+  done
+
+theorem inf_p_4k_plus_one (hp : p.Prime) (hp2 : p > 2) (hs : IsSquare (-1 : ZMod p)) : p = 4*k+1 ∧ exists_infinitely_many_P := by
   have hp_odd : Odd p := by
     {
       apply prime_gt_two_is_odd
       exact hp
       exact hp2
     }
+  have h_cong_1 : p % 4 = 1 := by
+    rw[← square_eq_neg_one_mod_p_iff_p_eq_one_mod_four]
+    assumption
+    assumption
+    assumption
+  have h_ex_k : (∃ (k : ℕ), p = 4*k + 1) := by
+  {
+    rw [← p_mod_4_eq_one_iff_p_eq_4k_plus_1']
+    assumption
+    assumption
+  }
   apply And.intro
+  case left =>
+    sorry
   case right =>
-    have h_cong_1 : p % 4 = 1 := by
-      rw[← square_eq_neg_one_mod_p_iff_p_eq_one_mod_four]
-      assumption
-      assumption
-      assumption
-    rw [← p_mod_4_eq_one_iff_p_eq_4k_plus_1]
-    assumption
-    assumption
-    -- apply Nat.exists_infinite_primes
-  sorry
+    sorry
+      -- apply Nat.exists_infinite_primes
   done
 
 end TPwLDirichlet
