4adb2d7e84607608dfe3fc601af0da0bf8c7e3a4
JamesRv21 <james.harvey@warwick.ac.uk>
2024-01-26 16:37:06 +0000

    Commiting to work on leons laptop

--- MA4N1Project/testingFunamental.lean
+++ MA4N1Project/testingFunamental.lean
@@ -20,59 +20,64 @@ set_option maxHeartbeats 0
 -- the cases which are proved in this namespace: 4k + 1, 6k + 1 and 8k + 1
 ---------------------------------------------------------------------------------------------------
 
-theorem james_trivial {f : ℤ[X]} {g : ℤ[X]} (hp : coeff f 0 = 0) : n ∣ (f.eval n) := by
-  have hp2 : f.eval 0 = 0 := by
-    rw[<-coeff_zero_eq_eval_zero]
-    exact hp
-    done
-  have hp3 : f = X * g := by
-    sorry
-    done
-  have hp4 : (X * g).eval n = n * g.eval n := by
-    simp_all only [mul_coeff_zero, coeff_X_zero, zero_mul, eval_mul, eval_X]
-    done
-  rw [hp3]
-  rw [hp4]
-  exact Int.dvd_mul_right n (eval n g)
-  done
+-- theorem james_trivial {f : ℤ[X]} {g : ℤ[X]} (hp : coeff f 0 = 0) : n ∣ (f.eval n) := by
+--   let a := coeff f 0
+--   let g := Polynomial.divX f
+--   have hp3' : f = X * g + C a := by
+--     exact (X_mul_divX_add f).symm
+--     done
 
-theorem primeJames (p : ℕ): (_root_.Prime p) ↔ (Nat.Prime p) := by
-  exact Iff.symm prime_iff
-  done
+--   have hp3 : f = X * g := by
+--     rw[hp3']
+--     simp only [eq_intCast, add_right_eq_self, Int.cast_eq_zero]
+--     exact hp
+--     done
+
+--   have hp4 : (X * g).eval n = n * g.eval n := by
+--     simp only [eval_mul, eval_X]
+--     done
+
+--   rw [hp3]
+--   rw [hp4]
+--   exact Int.dvd_mul_right n (eval n g)
+--   done
+
+-- theorem primeJames (p : ℕ): (_root_.Prime p) ↔ (Nat.Prime p) := by
+--   exact Iff.symm prime_iff
+--   done
 
 
-theorem trivial_case (M : ℕ) {f : ℤ[X]} (hp : coeff f 0 = 0) : ∃ p n, _root_.Prime p ∧ M ≤ p ∧ (p : ℤ) ∣ f.eval n :=
-  let p := minFac (M ! + 1)
-  let n := p
-  have f1 : M ! + 1 ≠ 1 := Nat.ne_of_gt <| succ_lt_succ <| factorial_pos _
-  have pp : Nat.Prime p := minFac_prime f1
-  have ppp : _root_.Prime p := by
-    exact (primeJames p).mpr pp
-  have hp2 : (p:ℤ) ∣ f.eval p := by
-    apply james_trivial hp
-    exact f
-    done
-  have np : M ≤ p :=
-    le_of_not_ge fun h =>
-      have h₁ : p ∣ M ! := dvd_factorial (minFac_pos _) h
-      have h₂ : p ∣ 1 := (Nat.dvd_add_iff_right h₁).2 (minFac_dvd _)
-      pp.not_dvd_one h₂
-  ⟨p, n, ppp, np, hp2⟩
+-- theorem trivial_case (M : ℕ) {f : ℤ[X]} (hp : coeff f 0 = 0) : ∃ p n, _root_.Prime p ∧ M ≤ p ∧ (p : ℤ) ∣ f.eval n :=
+--   let p := minFac (M ! + 1)
+--   let n := p
+--   have f1 : M ! + 1 ≠ 1 := Nat.ne_of_gt <| succ_lt_succ <| factorial_pos _
+--   have pp : Nat.Prime p := minFac_prime f1
+--   have ppp : _root_.Prime p := by
+--     exact (primeJames p).mpr pp
+--   have hp2 : (p:ℤ) ∣ f.eval p := by
+--     apply james_trivial hp
+--     exact f
+--     done
+--   have np : M ≤ p :=
+--     le_of_not_ge fun h =>
+--       have h₁ : p ∣ M ! := dvd_factorial (minFac_pos _) h
+--       have h₂ : p ∣ 1 := (Nat.dvd_add_iff_right h₁).2 (minFac_dvd _)
+--       pp.not_dvd_one h₂
+--   ⟨p, n, ppp, np, hp2⟩
 
-theorem if_min_fac_then_p_dvd_f (hp : p = minFac (f)) : p ∣ f := by
-  sorry
-  done
+
+-- theorem if_min_fac_then_p_dvd_f (hp : p = minFac (f)) : (p : ℤ) ∣ f := by
+--   rw [hp]
+--   exact minFac_dvd f
+--   done
 
 theorem non_trivial_case {f : ℤ[X]} {g : ℤ[X]} (hf : f.natDegree ≠ 0) (hp : coeff f 0 ≠ 0) (M : ℕ) : ∃ p n, _root_.Prime p ∧ M ≤ p ∧ (p : ℤ) ∣ f.eval n :=
   let a := coeff f 0
   let n := M ! * a ^ 2
+  let g := Polynomial.divX f
 
   have hp3 : f = X * g + C a := by
-    sorry
-    done
-
-  have simple : eval n X = n := by
-    exact eval_X
+    exact (X_mul_divX_add f).symm
     done
 
   have hp4 : f.eval n = n * g.eval n + a := by
@@ -105,7 +110,20 @@ theorem non_trivial_case {f : ℤ[X]} {g : ℤ[X]} (hf : f.natDegree ≠ 0) (hp 
 
   let functionAbsolute := Int.natAbs (a * (M !) * (g.eval (M ! * a ^ 2)) + 1)
   let p := minFac (functionAbsolute)
-  have f1 : functionAbsolute ≠ 1 := sorry
+
+  have hp9 : (p : ℤ) ∣ functionAbsolute := by
+    exact if_min_fac_then_p_dvd_f rfl
+    done
+
+  -- have f1 : functionAbsolute ≠ 1 := by
+  --   apply Nat.ne_of_gt
+
+  --   done
+  have f2 : a * (M !) * (g.eval (M ! * a ^ 2)) ≠ 0 := by
+    rw?
+    done
+
+
   have pp : Nat.Prime p := minFac_prime f1
 
   have np : M ≤ p :=
@@ -117,13 +135,15 @@ theorem non_trivial_case {f : ℤ[X]} {g : ℤ[X]} (hf : f.natDegree ≠ 0) (hp 
   have ppp : _root_.Prime p := by
     exact (primeJames p).mpr pp
 
-  have hp2 : p ∣ functionAbsolute := by
-    exact if_min_fac_then_p_dvd_f rfl
+  have hp10 : (functionAbsolute : ℤ) ∣ f.eval n := by
+    refine Int.natAbs_dvd.mpr ?_
+    rw[hp8]
+    apply hp7
     done
 
   have hp123 : ((p:ℤ) ∣ f.eval n) := by
-
+    exact Int.dvd_trans hp9 hp10
     done
 
 
-  ⟨p, n, ppp, np, hp2⟩
+  ⟨p, n, ppp, np, hp123⟩
